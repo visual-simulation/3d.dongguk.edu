d8e02020396eba016fdddfe0d3e674391dfb0e82
d_jglim <d_jglim@121da71d-aab0-eb4b-9dba-246f1b3e478e>
2015-08-07 10:48:29 +0000

    image textures for particle.

--- WEB3D/VFXLib/shaders/pointCloudFrag.glsl
+++ WEB3D/VFXLib/shaders/pointCloudFrag.glsl
@@ -8,7 +8,15 @@ uniform sampler2D texture;
 void main() {
 
     //gl_FragColor = vec4(1.0, 0.0, 0.0, 0.1);
-    gl_FragColor = texture2D(texture, gl_PointCoord);
+    vec4 c = texture2D(texture, gl_PointCoord);
+
+   // if(c.w < 0.1) discard;
+
+    gl_FragColor = c;
+
+
+
+    //gl_FragColor.w *= 0.1;
 
 
 }
--- WEB3D/VFXLib/shaders/pointCloudVert.glsl
+++ WEB3D/VFXLib/shaders/pointCloudVert.glsl
@@ -7,6 +7,10 @@ attribute float size;
 
 void main() {
 
-    gl_PointSize = 30.0;
+    //gl_PointSize = size;
+
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+    gl_PointSize = size*(300.0/length( mvPosition.xyz));
+
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
