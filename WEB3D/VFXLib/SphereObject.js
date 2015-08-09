f32dc8fd4f7d4671bc9f69bff3e1e756f14428a4
d_jglim <d_jglim@121da71d-aab0-eb4b-9dba-246f1b3e478e>
2015-08-09 07:15:17 +0000

    particles and objects interaction.

--- WEB3D/VFXLib/SphereObject.js
+++ WEB3D/VFXLib/SphereObject.js
@@ -4,19 +4,37 @@ function SphereObject() {
     var center;
     var radian;
 
+    this.initialize = function(cen, rad) {
+
+        center = cen;
+        radian = rad;
+    }
+
     this.collide = function(pos, vel) {
 
         var deviation = new THREE.Vector3();
-        deviation.subVectors(center, pos);
+        deviation.subVectors(pos, center);
 
         var length = deviation.length();
-        var dir = deviation.clone();
-        dir.normalize();
 
         if(length <= radian) {
+            // update position
 
-            pos.addVectors(center, dir.clone().multiplyScalar(radian));
+            var nor = deviation.clone().normalize();
+            var norVelMag = nor.clone().dot(vel);
 
+            pos.addVectors(center, nor.clone().multiplyScalar(radian));
+
+            if(norVelMag < 0.0) {
+                // update velocity
+
+                var norVel = nor.clone().multiplyScalar(norVelMag);
+                var tanVel = vel.clone().sub(norVel);
+
+                norVel.multiplyScalar(-1);
+
+                vel.addVectors(norVel.multiplyScalar(0.0), tanVel.multiplyScalar(1.0));
+            }
         }
     }
 }
