76f5dd2e670d9e454d176b81548798ac2d293aa8
d_shjang <d_shjang@121da71d-aab0-eb4b-9dba-246f1b3e478e>
2016-02-13 07:17:42 +0000

    change earthquake.

--- WEB3D/VFXLib/PostProcessor.js
+++ WEB3D/VFXLib/PostProcessor.js
@@ -6,6 +6,7 @@ var textureFlare3 = THREE.ImageUtils.loadTexture( "./textures/lensflare3.png" );
 function PostProcessor() {
 
     var _this = this;
+    var canvas = document.createElement('canvas');
 
     //snow
     var snowCanvas;
@@ -28,25 +29,39 @@ function PostProcessor() {
     var rainPositions;
     var rainLives;
     var rainSizes;
+    var rainColors;
+    var rainAlpha;
+    var rainWidths;
+    var rainHeights;
     var rainSeedSize;
-    var rainDrops;
+    var rainSeedLife;
+    var rainSpeeds;
+    var rainStops;
     var rainOptions;
-    var rainsClone;
-    var PRIVATE_GRAVITY_FORCE_FACTOR_Y;
     var PRIVATE_GRAVITY_FORCE_FACTOR_X;
-    var a;
 
+    var ox;
+    var oy;
+    var xe;
+    var ye;
+    var xm;
+    var ym;
+
+    //earthquake
+    var pre_x = 0;
+    var pre_y = 0;
+    var pre_z = 0;
 
     this.rain = function( _total ) {
 
-        rainCanvas = document.createElement('canvas');
+        rainCanvas = canvas;
         rainCanvas.width = window.innerWidth;
         rainCanvas.height = window.innerHeight;
 
         rainContext = rainCanvas.getContext('2d');
 
         rainOptions = {
-            opacity: 1,
+            opacity: 0.1,
             fps: 60,
             gravityThreshold: 3,
             gravityAngle: Math.PI / 2,
@@ -54,10 +69,10 @@ function PostProcessor() {
         };
 
 
-        PRIVATE_GRAVITY_FORCE_FACTOR_Y = (rainOptions.fps * 0.001) / 25;
         PRIVATE_GRAVITY_FORCE_FACTOR_X = ((Math.PI / 2) - rainOptions.gravityAngle) * (rainOptions.fps * 0.001) / 50;
 
-        rainSeedSize = 4;
+        rainSeedSize = 8;
+        rainSeedLife = 3;
 
         rainTotal = _total;
         rainCount = 0;
@@ -66,11 +81,22 @@ function PostProcessor() {
         rainPositions = new Float32Array(rainTotal*2);
         rainLives = new Float32Array(rainTotal * 1);
         rainSizes = new Float32Array(rainTotal * 1);
+        rainColors = new Array(rainTotal * 1);
+        rainAlpha = new Float32Array(rainTotal * 1);
+        rainWidths = new Float32Array(rainTotal * 1);
+        rainHeights = new Float32Array(rainTotal * 1);
+        rainSpeeds = new Float32Array(rainTotal * 1);
+        rainStops = new Float32Array(rainTotal * 1);
 
         return rainCanvas;
     }
 
     this.addRain = function( i, x, y ) {
+
+        var colors = [
+            "rgba(0, 0, 255)," + (Math.random() +.5) + ")",
+            "rgba(52, 152, 200,"+ (Math.random() + .5) +")",
+            "rgba(41, 128, 250,"+ (Math.random() + .5) +")"];
 
         if(i >= rainTotal) {
             return;
@@ -78,8 +104,14 @@ function PostProcessor() {
 
         var idx = i*2;
 
-        rainLives[i] = 1;
+        rainLives[i] = Math.random()*rainSeedLife;
         rainSizes[i] = Math.random()*rainSeedSize;
+        rainColors[i] = colors[Math.floor(Math.random()*3)];
+        rainAlpha[i] = Math.random()*0.3;
+        rainWidths[i] = Math.random()*3 + rainSeedSize;
+        rainHeights[i] = Math.random()*10 + rainSeedSize;
+        rainSpeeds[i] = 0.02 * Math.floor(rainSizes[i]*Math.random()*100);
+        rainStops[i] = Math.random()*rainTotal;
 
         rainPositions[idx+0] = x;
         rainPositions[idx+1] = y;
@@ -93,69 +125,131 @@ function PostProcessor() {
 
         rainContext.clearRect(0, 0, rainCanvas.width, rainCanvas.height);
 
-        rainContext.fillStyle = '#FFFAFA';
-        rainContext.globalAlpha = 1.0;
 
-        for(var i = 0; i < snowTotal; i++){
+        for(var i = 0; i < rainTotal; i++){
 
             var idx = i*2;
 
-            rainContext.beginPath();
-            rainContext.moveTo(rainPositions[idx + 0], rainPositions[idx + 1]);
-            rainContext.arc(rainPositions[idx + 0], rainPositions[idx + 1], rainSizes[i], 0, Math.PI * 2, true);
-            rainContext.closePath();
-            rainContext.fill();
+
+
+            rainContext.fillStyle = rainColors[i];
+
+            //rainContext.globalAlpha = 0.3;
+
+            if(rainSizes[i] < rainOptions.gravityThreshold){
+                rainContext.shadowBlur = 0;
+                rainContext.globalAlpha = 0.5;
+                rainContext.beginPath();
+                rainContext.moveTo(rainPositions[idx + 0], rainPositions[idx + 1]);
+                rainContext.arc(rainPositions[idx + 0], rainPositions[idx + 1], rainSizes[i], 0, Math.PI * 2, true);
+                rainContext.closePath();
+                rainContext.fill();
+            }else {
+
+                var x = rainPositions[idx+0];
+                var y = rainPositions[idx+1];
+                var w = rainWidths[i];
+                var h = rainHeights[i];
+
+                _this.bubbleCalculate(x,y,w,h);
+
+                rainContext.shadowBlur = 1;
+                rainContext.shadowColor="black";
+                rainContext.strokeStyle = rainColors[i];
+                rainContext.globalAlpha = 1;
+                rainContext.lineWidth = 0.1;
+
+                rainContext.beginPath();
+                rainContext.moveTo(x, ym);
+                rainContext.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+                rainContext.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+                rainContext.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+                rainContext.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+                rainContext.closePath();
+                rainContext.stroke();
+
+
+                rainContext.beginPath();
+                rainContext.moveTo(x, ym);
+                rainContext.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+                rainContext.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+                rainContext.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+                rainContext.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+                rainContext.closePath();
+                rainContext.stroke();
+
+                x += 2;
+                y += 1;
+
+                _this.bubbleCalculate(x,y,w-6,w-6);
+                rainContext.shadowBlur=0;
+                rainContext.fillStyle = 'white';
+                rainContext.lineWidth = 0.2;
+                rainContext.beginPath();
+                rainContext.moveTo(x, ym);
+                rainContext.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+                rainContext.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+                rainContext.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+                rainContext.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+                rainContext.closePath();
+                rainContext.fill();
+
+            }
 
         }
 
+    }
+
+    this.bubbleCalculate = function(x, y, w, h) {
+        var kappa = 0.5522848;
+        kappa = 0.65;
+
+        ox = (w / 2) * kappa;
+        oy = (h / 2) * kappa;
+        xe = x + w;
+        ye = y + h;
+        xm = x + w / 2;
+        ym = y + h / 2;
     }
 
     this.updateRain = function() {
-        _this.addRain();
-        rainsClone = rainDrops.slice();
-        var newRains = [];
-        for (var i = 0; i < rainsClone.length; ++i) {
-            if( rainsClone[i].animate()) {
-                newRains.push(rainsClone[i]);
+
+        for(var i=0; i < rainTotal; i++) {
+
+            var idx = i*2;
+
+            if(rainLives[i] > 0){
+
+                if(rainStops[i] > 0){
+                    rainStops[i] -= 0.1;
+                }
+                else {
+                    if(rainSizes[i] > rainOptions.gravityThreshold){
+
+                        rainPositions[idx+1] += rainSpeeds[i];
+                        rainSpeeds[i] -= 0.1;
+                        if(rainSpeeds[i] < 0) {
+                            rainSpeeds[i] = 0;
+                            rainStops[i] = Math.random()*rainTotal;
+                        }
+                        rainPositions[idx] += PRIVATE_GRAVITY_FORCE_FACTOR_X * Math.floor(rainSizes[i]);
+                    }
+                    else{
+                        rainLives[i] -= 0.001;
+                    }
+                    if(rainPositions[idx+1] > rainCanvas.height){
+                        rainLives[i] = 0;
+                    }
+                }
+
+            }
+            else{
+                _this.addRain(i, Math.random() * rainCanvas.width, Math.random() * rainCanvas.height);
             }
         }
-        rainDrops = newRains;
-    }
 
-    this.putRain = function(drop) {
-        drop.draw();
-        if(drop.r > rainOptions.gravityThreshold) {
-            rainDrops.push(drop);
-        }
-    }
+        _this.drawRain();
 
-    this.clearDrop = function(drop, force){
-        var result = drop.clear(force);
-        if (result) {
-            var index = rainDrops.indexOf(drop);
-            if (index >= 0) {
-                this.drops.splice(index, 1);
-            }
-        }
-        return result;
-    }
-
-    this.rainGravity = function(drop){
-        if (_this.clearDrop(drop)) {
-            return true;
-        }
-
-        if (drop.yspeed) {
-            drop.yspeed += this.PRIVATE_GRAVITY_FORCE_FACTOR_Y * Math.floor(drop.r);
-            drop.xspeed += this.PRIVATE_GRAVITY_FORCE_FACTOR_X * Math.floor(drop.r);
-        } else {
-            drop.yspeed = this.PRIVATE_GRAVITY_FORCE_FACTOR_Y;
-            drop.xspeed = this.PRIVATE_GRAVITY_FORCE_FACTOR_X;
-        }
-
-        drop.y += drop.yspeed;
-        drop.draw();
-        return false;
     }
 
 
@@ -203,47 +297,41 @@ function PostProcessor() {
 
 
 
-    this.earthquake = function(camera, sx, sy, sz) {
-
-        var force_x;
-        var force_y;
-        var force_z;
-
-
-        if(sx) {
-            force_x = sx;
-        }
-        else {
-            force_x = 10;
-        }
-
-        if(sy) {
-            force_y = sy;
-        }
-        else {
-            force_y = 10;
-        }
-
-        if(sz) {
-            force_z = sz;
-        }
-        else {
-            force_z = 0;
-        }
+    this.earthquake = function( _camera, force_x, force_y, force_z) {
 
         var cx = (0.5  - Math.random()) * force_x;
         var cy = (0.5  - Math.random()) * force_y;
         var cz = (0.5  - Math.random()) * force_z;
 
-        var change_camera = camera;
-        change_camera.position.set(change_camera.position.x + cx, change_camera.position.y + cy, change_camera.position.z + cz);
+        var change_camera = _camera;
+
+        change_camera.position.set(change_camera.position.x - pre_x + cx, change_camera.position.y - pre_y + cy, change_camera.position.z - pre_z + cz);
+
+        pre_x = cx;
+        pre_y = cy;
+        pre_z = cz;
 
         return change_camera;
+    };
+
+    this.changeState = function( state ){
+
+        if(state) {
+            pre_x = 0;
+            pre_y = 0;
+            pre_z = 0;
+            return false;
+        }
+        else {
+            return true;
+        }
+
+
     }
 
     this.snow = function( _total ) {
 
-        snowCanvas = document.createElement('canvas');
+        snowCanvas = canvas;
         snowCanvas.width = window.innerWidth;
         snowCanvas.height = window.innerHeight;
 
@@ -273,7 +361,7 @@ function PostProcessor() {
         for(var i=0; i < snowTotal; i++) {
             if(snowSizes[i] > 0.05 && snowLives[i] > 0.0) {
                 snowLives[i] -= dt;
-                snowSizes[i] -= 0.002;
+                snowSizes[i] -= (Math.random() * 0.08);
                 if(snowSizes[i] < 0.1) {
                     _this.addSnows(i, Math.random() * snowCanvas.width, Math.random() * snowCanvas.height);
                 }
@@ -293,7 +381,7 @@ function PostProcessor() {
         var idx = i*2;
 
         snowLives[i] = Math.random()*snowSeedLife;
-        snowSizes[i] = Math.random()*snowSeedSize;
+        snowSizes[i] = Math.random()*snowSeedSize+3;
 
         snowPositions[idx+0] = x;
         snowPositions[idx+1] = y;
@@ -307,7 +395,9 @@ function PostProcessor() {
         snowCtx.clearRect(0, 0, snowCanvas.width, snowCanvas.height);
 
         snowCtx.fillStyle = '#FFFAFA';
-        snowCtx.globalAlpha = 1.0;
+        snowCtx.globalAlpha = 0.8;
+        snowCtx.shadowBlur = 5;
+        snowCtx.shadowColor="white";
 
         for(var i = 0; i < snowTotal; i++){
 
@@ -324,78 +414,3 @@ function PostProcessor() {
     }
 
 }
-
-/**
- * Defines a new raindrop object.
- * @param rainyday reference to the parent object
- * @param centerX x position of the center of this drop
- * @param centerY y position of the center of this drop
- * @param min minimum size of a drop
- * @param base base value for randomizing drop size
- */
-
-function Drop(rainy, centerX, centerY, min, base) {
-    this.x = Math.floor(centerX);
-    this.y = Math.floor(centerY);
-    this.r = (Math.random() * base) + min;
-    this.rainy = rainy;
-    this.canvas = rainy.getRainCanvas();
-    this.context = rainy.getRainContext();
-}
-
-/**
- * Draws a raindrop on canvas at the current position.
- */
-Drop.prototype.draw = function() {
-    this.context.save();
-    this.context.beginPath();
-
-    var orgR = this.r;
-    this.r = 0.95 * this.r;
-    if (this.r < 3) {
-        this.context.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
-        this.context.closePath();
-    } else {
-        this.context.arc(this.x, this.y, this.r * 0.9, 0, Math.PI * 2, true);
-        this.context.closePath();
-    }
-
-    this.context.clip();
-
-    this.r = orgR;
-
-    this.context.fillStyle = '#8ED6FF';
-    this.context.fill();
-
-    this.context.restore();
-};
-
-/**
- * Clears the raindrop region.
- * @param force force stop
- * @returns Boolean true if the animation is stopped
- */
-Drop.prototype.clear = function(force) {
-    this.context.clearRect(this.x - this.r - 1, this.y - this.r - 2, 2 * this.r + 2, 2 * this.r + 2);
-    if (force) {
-        this.terminate = true;
-        return true;
-    }
-    if ((this.y - this.r > this.canvas.height) || (this.x - this.r > this.canvas.width) || (this.x + this.r < 0)) {
-        // over edge so stop this drop
-        return true;
-    }
-    return false;
-};
-
-/**
- * Moves the raindrop to a new position according to the gravity.
- */
-Drop.prototype.animate = function() {
-    if (this.terminate) {
-        return false;
-    }
-    var stopped = this.rainy.rainGravity(this);
-
-    return !stopped || this.terminate;
-};
